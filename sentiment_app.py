8bdb3609ca7f0ca95e8d9a50ebb6d4d53ef20c65
Pulipaka Pavan Kumar <122916232+Protagonist-8@users.noreply.github.com>
2025-08-23 19:29:09 +0530

    Initial commit, with classical ML model

---
+++ sentiment_app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import re
+import joblib
+
+# Load model + vectorizer
+model = joblib.load("sentiment_model.pkl")
+tfidf = joblib.load("tfidf_vectorizer.pkl")
+port_stem=PorterStemmer()
+# Preprocessing function (same as training!)
+def stemming(content):
+  stemmed_content=re.sub('[^a-zA-Z]',' ',content)
+  stemmed_content=stemmed_content.lower()
+  stemmed_content=stemmed_content.split()
+  stemmed_content=[port_stem.stem(word) for word in stemmed_content if word not in stopwords.words('english')]
+  stemmed_content=' '.join(stemmed_content)
+
+  return stemmed_content
+
+# Streamlit UI
+st.title("Sentiment Analysis App")
+
+user_input = st.text_area("Enter text here:")
+
+if st.button("Predict"):
+    if user_input.strip():
+        # Preprocess
+        cleaned_text = stemming(user_input)
+
+        # Feature extraction
+        features = tfidf.transform([cleaned_text])
+
+        # Prediction
+        pred = model.predict(features)[0]
+        prob = model.predict_proba(features)[0]
+
+        st.write(f"**Prediction:** {pred}")
+        st.write(f"**Confidence:** {max(prob):.2f}")
+    else:
+        st.write("Please enter some text.")
